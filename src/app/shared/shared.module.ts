f76ee7b733441bae4be2dd6b19c2c4057d9e719f
Andy Pelaez <andy.pelaez@ntsprint.com>
2022-11-16 12:16:06 -0500

    created generic list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GenericTableComponent } from './generic-table/generic-table.component';
-import { TableComponent } from './table/table.component';
 import { InputComponent } from './input/input.component';
 import { ButtonComponent } from './button/button.component';
+import { GenericListComponent } from './generic-list/generic-list.component';
 
 @NgModule({
-  declarations: [InputComponent, ButtonComponent],
+  declarations: [
+    InputComponent,
+    ButtonComponent,
+    GenericListComponent,
+  ],
   imports: [CommonModule],
-  exports: [InputComponent, ButtonComponent],
+  exports: [
+    InputComponent,
+    ButtonComponent,
+    GenericListComponent
+  ],
 })
 export class SharedModule {}
